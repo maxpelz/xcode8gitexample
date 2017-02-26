9cb49f886ded1adedc81d487c6a3fced15d1c97a
Max Pelz <maxpelz@bernadesmacbook>
2017-02-26 11:49:07 +0100

    Initial Commit

---
+++ xcode8gitexample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  xcode8gitexample
+//
+//  Created by Max Pelz on 26.02.17.
+//  Copyright © 2017 Max Pelz. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ xcode8gitexample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  xcode8gitexample
+//
+//  Created by Max Pelz on 26.02.17.
+//  Copyright © 2017 Max Pelz. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
